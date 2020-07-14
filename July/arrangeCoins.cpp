165dd63a407f744eea24399fcac12e57f894fbbf
Berry-GUO <berry.0324@g.ncu.edu.tw>
2020-07-14 11:54:28 +0800

    first question clear

--- July/arrangeCoins.cpp
+++ July/arrangeCoins.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        
+        int rows = 1;
+        // int sum = 0;
+        int result = n;
+
+        while(true)
+        {
+            result = result - rows;
+
+            if(result == 0)
+            {
+                break;
+            }
+            else if (result < 0)
+            {
+                rows--;
+                break;
+            }
+            else
+            {
+                rows = rows + 1;                
+            }
+            
+
+            /*
+            *the following code will overflow
+            */
+
+            // sum = sum + rows;
+            // if(n == sum)
+            // {
+            //     break;
+            // }
+            // else if (n < sum)
+            // {
+            //     rows--;
+            //     break;
+            // }            
+            // else
+            // {
+            //     rows = rows + 1;
+            // }
+        }
+
+        return rows;
     }
 };
